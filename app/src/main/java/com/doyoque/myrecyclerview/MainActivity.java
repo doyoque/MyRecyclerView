f794e01de41120c05351b35a4991729169d571d4
yoga <abiyoga1335@gmail.com>
2020-04-25 15:40:21 +0700

    grid recycle views

--- app/src/main/java/com/doyoque/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/doyoque/myrecyclerview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.doyoque.myrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.doyoque.myrecyclerview.adapter.GridHeroAdapter;
 import com.doyoque.myrecyclerview.adapter.ListHeroAdapter;
 import com.doyoque.myrecyclerview.model.Hero;
 import com.doyoque.myrecyclerview.model.HeroesData;
@@ -36,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setAdapter(listHeroAdapter);
     }
 
+    private void showRecyclerGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -51,8 +59,10 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                showRecyclerList();
                 break;
             case R.id.action_grid:
+                showRecyclerGrid();
                 break;
             case R.id.action_cardview:
                 break;
